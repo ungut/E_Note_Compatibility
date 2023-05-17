35add4733d28e20c7388e199cf086b18f2e4ca5a
Aziz <Aziz.ungut@outlook.com>
2023-05-17 15:16:23 +0100

    Added Scrollbar

--- Enote.py
+++ Enote.py
@@ -5,11 +5,13 @@ from Cryptodome.Random import get_random_bytes
 from Cryptodome.Hash import SHA256
 from base64 import b64encode
 import tkinter as tk
+import tkinter as ttk
 from tkinter.filedialog import askopenfilename
 from PIL import Image, ImageTk
 import json
 import uuid
 import sys
+from tkmacosx import Button
 BG_COLOR = "#346466"
 WIN_WIDTH = 600
 WIN_HEIGHT = 600
@@ -87,9 +89,29 @@ root = tk.Tk()
 root.title("Enote")
 root.eval("tk::PlaceWindow . center")
 
+#çreate a Canvas
 master_frame = tk.Frame(root, width=WIN_WIDTH / 3, height=WIN_HEIGHT, bg=BG_COLOR)
-master_frame.pack(side="left", fill="y", expand="no")
+master_frame.pack(side="left", fill="y", expand=0)
 master_frame.pack_propagate(False)
+
+master_canvas = tk.Canvas(master_frame)
+master_canvas.pack(side="left",fill="both",expand=1)
+
+#Add A Scrollbar To The Canvas
+master_scrollbar =  ttk.Scrollbar(root,orient="vertical",command=master_canvas.yview,bg=BG_COLOR)
+master_scrollbar.pack(side="left",fill="y")
+
+#Configure The Canvas
+master_canvas.configure(yscrollcommand=master_scrollbar.set)
+master_canvas.bind("<Configure>",lambda e:  master_canvas.configure(scrollregion=master_canvas.bbox("all")))
+
+#Create A Second Frame INSIADE The Canvas
+second_frame = tk.Frame(master_canvas)
+
+#Add That Frame To a Window In The Canvas
+master_canvas.create_window((0,0),window=second_frame,anchor="nw")
+
+
 
 detail_frame = tk.Frame(
     root,
@@ -99,6 +121,8 @@ detail_frame = tk.Frame(
     highlightbackground="green",
     highlightthickness=2,
 )
+def load_detail_frame():
+    print("hello")
 
 
 detail_frame.pack(side="left",fill="both",expand="yes")
@@ -110,11 +134,28 @@ resized_image = img.resize((30, 30), Image.LANCZOS)
 logo_image = ImageTk.PhotoImage(resized_image)
 logo_widget = tk.Label(master_frame, image=logo_image, bg="#346466")
 logo_widget.image = logo_image
-logo_widget.pack(anchor="w")
+#logo_widget.pack(anchor="w")
+def generate_master_view(items):
+    for i,item in enumerate(items):
+        text = item["Title"]
+        Button(
+            second_frame,
+            text= text,
+            width=(master_frame.winfo_width()-10),
+            anchor="w",
+            font=("TkHeadingFont",15),
+            bg="#28393a",
+            fg="white",
+            cursor="hand2",
+            activebackground="#badee2",
+            activeforeground="black",
+            command = lambda: load_detail_frame()
+        ).grid(row=i+2,column=0,sticky="w",pady=5)
 
 
 
 items = import_enote_items_struct()
+generate_master_view(items)
 #write_enote_items(items, "dummy.enote", "aa", "bb")
 #  read_enote_file("dummy.enote", "aa", "bb")
 # for item in items:
